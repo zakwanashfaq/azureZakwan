515c3601cd1528740d55d6a5881af7ebcb38ee1a
Zakwan Ashfaq Zian <zakwanashfaq@gmail.com>
2020-06-21 08:33:18 -0230

    3 git push

--- zian/views.py
+++ zian/views.py
@@ -3,4 +3,5 @@ from django.http import request, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Backend good!")
+    return HttpResponse("Backend good!"
+                        "<h1><p>Congrats on deployment to azure</p></h1>")
